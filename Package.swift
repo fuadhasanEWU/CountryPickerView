2afa854eda1b6dcd4641b07f1c15fa736435795f
fuadhasanEWU <48385095+fuadhasanEWU@users.noreply.github.com>
2025-02-22 15:01:24 +0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "CountryPickerView",
+        platforms: [
+        .iOS(.v14)  // Change this to the desired minimum version
+    ],
     products: [
         .library(name: "CountryPickerView", targets: ["CountryPickerView"])
     ],
